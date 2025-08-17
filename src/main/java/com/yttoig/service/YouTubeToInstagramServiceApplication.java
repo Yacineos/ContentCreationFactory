6a6adf372114d1ed7bf62ef49f1113e0cfc97b46
OUKKAL Yacine <psmartyacine@gmail.com>
2025-08-17 13:11:45 +0200

    Initial commit

---
+++ src/main/java/com/yttoig/service/YouTubeToInstagramServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yttoig.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeToInstagramServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeToInstagramServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yttoig/service/YouTubeToInstagramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yttoig.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeToInstagramServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
